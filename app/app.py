864d8deff120eda10b097fa269bd1f10797c6afd
Adhrit Sigdel <adhritsigdel@gmail.com>
2026-02-19 23:45:09 +0545

    first commit

---
+++ app/app.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Path, HTTPException
+
+app = FastAPI()
+
+
+
+students = {
+    1:{
+        "Name":"Adhrit",
+        "Class":11,
+        "Skill":"Greatest Programmer"
+    },
+    2:{
+        "Name":"Luffy",
+        "Class":1,
+        "Skill":"Greatest Pirate"
+    },
+    3:{
+        "Name":"Robin",
+        "Class":111,
+        "Skill":"Genius in Histry"
+    }
+
+}
+
+@app.get("/students/{student_id}")
+def students_data(student_id:int = Path(..., gt=0, description="Please Enter the id")):
+    if student_id not in students:
+        raise HTTPException(status_code=404, detail="You entered the wrong id")
+    else:
+        return students[student_id]
+
+@app.get("/students")
+def student_name(name:str):
+    for student_id in students:
+        if students[student_id]["Name"] == name:
+            return students[student_id]
+        
+        raise HTTPException(status_code=404, detail="You entered the wrong id")
